34c9303ec1596f3e142ad206b1e068a221842264
SACTel Cloud <sactel@gmail.com>
2024-08-01 20:22:31 -0500

    don Lolo Agosto 01 !

--- res/shared/archivo_script.php
+++ res/shared/archivo_script.php
@@ -7,6 +7,7 @@
 <!-- <script src="<?= BASE_JS ?>pdf-lib.js" type="text/javascript"></script> -->
 <!-- <script src="https://unpkg.com/pdf-lib@1.4.0"></script> -->
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="<?= BASE_JS ?>tomarfoto.js" type="text/javascript"></script>
 <script src="<?= BASE_JS ?>sactel.js" type="text/javascript"></script>
 <script src="<?= BASE_CSS ?>sweetalert.min.js" type="text/javascript" charset="utf-8" async defer></script>
 <script src="<?= BASE_RES ?>dist/app.min.js"></script>
